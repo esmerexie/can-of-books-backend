e47466e1641c452fa513a6205ca6ecb23852de3c
Austin Limanek <austin.limanek@gmail.com>
2022-07-26 19:55:55 -0700

    added the ability to add and delete books from the database

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const app = express();
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL);
 app.use(cors());
-const Book = require('./book.js')
+const Book = require('./models/book.js')
+app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
@@ -39,6 +40,47 @@ app.get('/books', (request, response) => {
 
 });
 
+app.post('/book', (request, response, next) => {
+  console.log(request.body);
+  let {title, description, status} = request.body;
+  if(!title || !description || !status){
+    next('Bad request')
+  }
+
+  let book = new Book({
+    title,
+    description,
+    status
+  });
+  book.save()
+    .then(results => {
+      response.send(results);
+    })
+    .catch(e => {
+      next(e);
+    })
+})
+
+app.delete('/book/:id', async (request, response, next) => {
+  let id = request.params.id;
+  try{
+    let book = await Book.deleteOne({_id: id});
+    response.send(book);
+  } catch(e){
+    next(e);
+  }
+})
+
+app.get('/book/:title/:description', (request, response, next) => {
+  console.log(typeof request.params.title);
+  console.log(request.params.description);
+  console.log(request.params)
+  response.send('Also working on it');
+})
+
+app.use((error, request, response, next) => {
+  response.status(500).send(error);
+})
 
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
